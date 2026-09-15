743aa955e38e707ef91f4481d22285078584ab47
itxzabdulhadi-hue <itxzabdulhadi@gmail.com>
2026-09-15 18:46:52 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createApp } from './server/app.ts';
-import { bootstrapAdmin } from './server/bootstrap.ts';
-import { assertProductionConfig } from './server/env.ts';
-import { sessionSecret } from './server/secret.ts';
-import { getStorage } from './server/storage/index.ts';
+import { createApp } from './server/app';
+import { bootstrapAdmin } from './server/bootstrap';
+import { assertProductionConfig } from './server/env';
+import { sessionSecret } from './server/secret';
+import { getStorage } from './server/storage/index';
 
 assertProductionConfig();
 
@@ -11,6 +11,6 @@ await getStorage().init();
 await bootstrapAdmin();
 await sessionSecret();
 
-const app: express.Express = createApp({ serveStatic: true });
+const app = createApp({ serveStatic: true });
 
 export default app;
