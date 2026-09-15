fbb27abc6593eb4a88b7b13e5e6ce269dc731330
itxzabdulhadi-hue <itxzabdulhadi@gmail.com>
2026-09-15 18:56:39 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,15 +1,5 @@
 import 'express';
 import { createApp } from './server/app.js';
-import { bootstrapAdmin } from './server/bootstrap.js';
-import { assertProductionConfig } from './server/env.js';
-import { sessionSecret } from './server/secret.js';
-import { getStorage } from './server/storage/index.js';
-
-assertProductionConfig();
-
-await getStorage().init();
-await bootstrapAdmin();
-await sessionSecret();
 
 const app = createApp({ serveStatic: true });
 
